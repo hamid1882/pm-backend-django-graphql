9dc66e95ac2e5143e111b43344d8d56c689c03a6
hamid1882 <hussainhafeez1100@gmail.com>
2025-12-14 18:58:48 +0530

    fix: add production URL to CORS_ALLOWED_ORIGINS in settings.py

--- pm_api/settings.py
+++ pm_api/settings.py
@@ -139,6 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://localhost:5173",
+    "https://pm-frontend-lake.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
