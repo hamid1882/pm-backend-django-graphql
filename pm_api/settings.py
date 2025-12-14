e6cd9d4045d25481afcc5ff6784fad0707fb1309
hamid1882 <hussainhafeez1100@gmail.com>
2025-12-14 15:29:03 +0530

    fix: update CORS_ALLOWED_ORIGINS to specify local development URLs in settings.py

--- pm_api/settings.py
+++ pm_api/settings.py
@@ -136,7 +136,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = "*"
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://localhost:5173",
+]
 
 CORS_ALLOW_CREDENTIALS = True
 
